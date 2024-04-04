830c790da36d29540a4dbc9b6891a2d5be1c030d
PanchoGmZ <fg08723>
2024-04-04 16:59:16 -0400

    creacion de bibliotecas clases

---
+++ SistemaGestionVentas/GestionVentas.Modelos/Ventas.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionVentas.Modelos
+{
+    public class Ventas
+    {
+        public int IdVentas { get; set; }
+        public DateTime FechaVenta { get; set; }
+        public decimal TotalVenta { get; set; }
+
+    }
+}
